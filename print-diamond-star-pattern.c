41ed915b4e2ec3e21b27e23913ad4ad12c5dbe83
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-13 09:35:37 +0530

    Create print-diamond-star-pattern.c

---
+++ print-diamond-star-pattern.c
@@ -0,0 +1,48 @@
+/*
+Author: Aishwarya
+Email: aishucool34@gmail.com
+Problem Statement 8:Draw the Diamond pattern
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+Diamond Star Pattern
+*/
+#include<stdio.h>
+void printDiamond(int n)
+{
+    int space = n - 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0;j < space; j++)
+            printf(" ");
+        for (int j = 0;j <= i; j++)
+            printf("* ");
+
+        printf("\n");
+        space--;
+    }
+    space = 0;
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 0; j < space; j++)
+            printf(" ");
+        for (int j = 0;j < i;j++)
+            printf("* ");
+
+        printf("\n");
+        space++;
+    }
+}
+int main()
+{
+    printDiamond(5);
+    return 0;
+}
