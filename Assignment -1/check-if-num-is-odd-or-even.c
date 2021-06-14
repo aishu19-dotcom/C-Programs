5d13dedd4dd90d99022ee2507f2e8bd4a7d077ee
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-14 20:11:22 +0530

    Create check-if-num-is-odd-or-even.c

---
+++ Assignment -1/check-if-num-is-odd-or-even.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+
+    int num;
+
+    printf("Enter an integer number: ");
+    scanf("%d",&num);
+    if(num%2==0)
+        printf("%d is an EVEN number.",num);
+    else
+        printf("%d is an ODD number.",num);
+
+    return 0;
+}
