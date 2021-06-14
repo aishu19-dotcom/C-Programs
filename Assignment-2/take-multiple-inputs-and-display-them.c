014e14901ed97e61b5ee423b28a2853611e94a37
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-14 21:08:45 +0530

    Create take-multiple-inputs-and-display-them.c

---
+++ Assignment-2/take-multiple-inputs-and-display-them.c
@@ -0,0 +1,18 @@
+/*
+Author: Aishwarya
+program 1: Write a C Program to take multiple inputs from the user and display them.
+*/
+#include <stdio.h>
+int main()
+{
+    int a;
+    float b;
+
+    printf("Enter integer and then a float: ");
+
+    // Taking multiple inputs
+    scanf("%d%f", &a, &b);
+
+    printf("You entered %d and %f", a, b);
+    return 0;
+}
