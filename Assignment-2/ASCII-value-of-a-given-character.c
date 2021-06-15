f00099065e171b1ec9b82017fcc108e82ef5b459
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-15 13:58:25 +0530

    Update ASCII-value-of-a-given-character.c

--- Assignment-2/ASCII-value-of-a-given-character.c
+++ Assignment-2/ASCII-value-of-a-given-character.c
@@ -1,14 +1,4 @@
 /*
 Author: Aishwarya
-Program 5: Write a C program to get a ASCII value of a given character?
+Program 6: Write a C program to read a string using gets() function and print it on the screen using printf() function.
 */
-
-#include <stdio.h>
-int main()
-{
-    char ch;
-    printf("Enter a character");
-    scanf("%c",&ch);  
-    printf("\n The ascii value of the ch variable is : %d", ch);
-    return 0;
-}
