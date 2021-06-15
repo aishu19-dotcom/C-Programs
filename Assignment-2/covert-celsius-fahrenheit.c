94a2db19b39eed303a93d4fba82cbea234cedf4a
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-15 14:05:06 +0530

    Update covert-celsius-fahrenheit.c

--- Assignment-2/covert-celsius-fahrenheit.c
+++ Assignment-2/covert-celsius-fahrenheit.c
@@ -1,15 +1,34 @@
 /*
 Author: Aishwarya
-Program 7: Write a C program to convert Celsius into Fahrenheit?
+Program 8: Write a C program to find the slope and midpoint of a given line segment?
 */
 
-#include <stdio.h>
-int main()
+#include<stdio.h>
+#include<conio.h>
+
+void main()
 {
-    float celsius, fahrenheit;
-    printf("Enter temperature in Celsius: ");
-    scanf("%f", &celsius);
-    fahrenheit = (celsius * 9 / 5) + 32;
-    printf("%.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
-    return 0;
+    float x1,x2,y1,y2,slope,midX,midY;
+    clrscr();
+
+    printf(" Enter the X Coordinate of Endpoint 1: ");
+    scanf("%f",&x1);
+    printf(" Enter the Y Coordinate of Endpoint 1: ");
+    scanf("%f",&y1);
+
+    printf(" Enter the X Coordinate of Endpoint 2: ");
+    scanf("%f",&x2);
+    printf(" Enter the Y Coordinate of Endpoint 2: ");
+    scanf("%f",&y2);
+
+    printf(" The Endpoints of a Line are : (%.2f,%.2f) and (%.2f,%.2f)",x1,y1,x2,y2);
+
+    slope=(y2-y1)/(x2-x1);
+
+    midX=(x1+x2)/2;
+    midY=(y1+y2)/2;
+
+    printf(" Slope : %.2f",slope);
+    printf(" Midpoint : (%.2f,%.2f)",midX,midY);
+    getch();
 }
