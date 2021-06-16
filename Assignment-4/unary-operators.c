7dbe239a8161b5c3466964892ea9bcfc7fcf9bf7
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-16 21:02:39 +0530

    Create unary-operators.c

---
+++ Assignment-4/unary-operators.c
@@ -0,0 +1,23 @@
+/*
+Question:Write a C Program using unary arithmetic Operators
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 4, res;
+    printf("a is %d and b is %d\n", a, b);
+    res = a + b; 
+    printf("a+b is %d\n", res);
+    res = a - b;
+    printf("a-b is %d\n", res);
+    res = a * b;
+    printf("a*b is %d\n", res);
+    res = a / b;
+    printf("a/b is %d\n", res);
+    res = a % b;
+    printf("a%%b is %d\n", res);
+
+    return 0;
+}
