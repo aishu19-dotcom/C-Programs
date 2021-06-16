5c33782aa22dc561b0d501a2936acc54f5a3ee44
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-16 20:57:39 +0530

    Update perform-arithmetic-operations.c

--- Assignment-4/perform-arithmetic-operations.c
+++ Assignment-4/perform-arithmetic-operations.c
@@ -9,11 +9,11 @@ int main()
  int a = 12, b = 3;
  int addition, subtraction, multiplication, division, modulus;
 
- addition = a + b; //addition of 3 and 12
- subtraction = a - b; //subtract 3 from 12
- multiplication = a * b; //Multiplying both
- division = a / b; //dividing 12 by 3 (number of times)
- modulus = a % b; //calculation the remainder
+ addition = a + b;
+ subtraction = a - b;
+ multiplication = a * b;
+ division = a / b;
+ modulus = a % b; 
 
  printf("Addition of two numbers a, b is : %d\n", addition);
  printf("Subtraction of two numbers a, b is : %d\n", subtraction);
