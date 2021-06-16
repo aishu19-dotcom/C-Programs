fd1dd492621bae8def25ea913565b7179c86311c
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-16 22:12:36 +0530

    Create swapping-2-bits-of-a-byte-using-bitwise-operator.c

---
+++ Assignment-4/swapping-2-bits-of-a-byte-using-bitwise-operator.c
@@ -0,0 +1,20 @@
+/*
+Question: Write a c program to swapping two bits of a byte using bitwise operators.
+*/
+
+#include<stdio.h>
+int swapBits(unsigned int n, unsigned int p1, unsigned int p2)
+{
+    unsigned int bit1 =  (n >> p1) & 1;
+    unsigned int bit2 =  (n >> p2) & 1;
+    unsigned int x = (bit1 ^ bit2);
+    x = (x << p1) | (x << p2);
+       two sets are swapped;
+    unsigned int result = n ^ x;
+}
+int main()
+{
+    int res =  swapBits(28, 0, 3);
+    printf("Result = %d ", res);
+    return 0;
+}
