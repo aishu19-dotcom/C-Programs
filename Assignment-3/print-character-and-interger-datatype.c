aa3ceed4c6f4a9f0c73cca4f5dc1f8820a21b628
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-16 19:11:50 +0530

    Create print-character-and-interger-datatype.c

---
+++ Assignment-3/print-character-and-interger-datatype.c
@@ -0,0 +1,15 @@
+/*
+Question:Accept an integer and a character as inputs. Add the integer to the character variable
+and output the result in character and integer data type.
+*/
+
+#include <stdio.h>
+int main() {
+    int number;
+
+    printf("Enter an integer: ");  
+    scanf("%d", &number);
+    printf("You entered: %d", number);
+
+    return 0;
+}
