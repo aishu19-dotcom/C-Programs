4a2e9d037e05d8c4b6c5e9ab4cc4b197dbaff493
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-17 12:30:06 +0530

    Create check-if-its-an-amstrong-number.c

---
+++ Assignment-5/check-if-its-an-amstrong-number.c
@@ -0,0 +1,23 @@
+/*
+Question : Write a program in C to check whether the given number is Armstrong number?
+*/
+
+#include<stdio.h>
+ int main()
+{
+int n,r,sum=0,temp;
+printf("enter the number=");
+scanf("%d",&n);
+temp=n;
+while(n>0)
+{
+r=n%10;
+sum=sum+(r*r*r);
+n=n/10;
+}
+if(temp==sum)
+printf("armstrong  number ");
+else
+printf("not armstrong number");
+return 0;
+}   
