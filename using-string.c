546116f93bb08256389dfdbbaa022d8b4312f68c
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-17 14:38:35 +0530

    Create using-string.c

---
+++ using-string.c
@@ -0,0 +1,12 @@
+/*
+Question: Write a basic c program using string
+*/
+#include <stdio.h>
+int main()
+{
+    char name[20];
+    printf("Enter name: ");
+    scanf("%s", name);
+    printf("Your name is %s.", name);
+    return 0;
+}
