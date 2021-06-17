8c94d76b2d62f5eb6f2c8bbc058b0ad89d69fece
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-17 13:07:30 +0530

    Create display-input-or-output-data-of-an-array.c

---
+++ Assignment-6/display-input-or-output-data-of-an-array.c
@@ -0,0 +1,20 @@
+/*
+Question:  Write a C program to display Input / Output data of array
+*/
+
+#include <stdio.h>
+
+int main() {
+  int values[5];
+
+  printf("Enter 5 integers: ");
+  for(int i = 0; i < 5; ++i) {
+     scanf("%d", &values[i]);
+  }
+
+  printf("Displaying integers: ");
+  for(int i = 0; i < 5; ++i) {
+     printf("%d\n", values[i]);
+  }
+  return 0;
+}
