068c88dee08688f6aadfb8ff08bb29d899afdc81
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-17 15:11:46 +0530

    Create reverse-a-string-using-recursion-function.c

---
+++ Assignment-8/reverse-a-string-using-recursion-function.c
@@ -0,0 +1,29 @@
+/*
+Question: Write a C program to Reverse a String using recursion
+*/
+
+#include <stdio.h>
+int main()
+{
+   char s[1000], r[1000];
+   int begin, end, count = 0;
+
+   printf("Input a string\n");
+   gets(s);
+
+   while (s[count] != '\0')
+      count++;
+
+   end = count - 1;
+
+   for (begin = 0; begin < count; begin++) {
+      r[begin] = s[end];
+      end--;
+   }
+
+   r[begin] = '\0';
+
+   printf("%s\n", r);
+
+   return 0;
+}
