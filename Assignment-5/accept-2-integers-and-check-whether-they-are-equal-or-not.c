26995e0c68e3fea92b1a02b307a57b6abf62b9ea
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-17 12:05:32 +0530

    Create accept-2-integers-and-check-whether-they-are-equal-or-not.c

---
+++ Assignment-5/accept-2-integers-and-check-whether-they-are-equal-or-not.c
@@ -0,0 +1,16 @@
+/*
+Question :Write a C program to accept two integers and check whether they are equal or not.
+*/
+
+#include <stdio.h>
+void main()
+{
+    int m, n;
+
+    printf("Enter the values for M and N\n");
+    scanf("%d %d", &m, &n);
+    if (m == n)
+        printf("M and N are equal\n");
+    else
+        printf("M and N are not equal\n");
+}
