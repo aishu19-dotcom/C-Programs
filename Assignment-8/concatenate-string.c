34f2602df3e6ec9c239c2e66ee4f8261ac2a3f1e
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-17 14:54:17 +0530

    Create concatenate-string.c

---
+++ Assignment-8/concatenate-string.c
@@ -0,0 +1,18 @@
+/*
+Question: Write a C Program to concatenate 2 strings
+*/
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+  char s1[20];
+  char s2[20]; 
+printf("Enter the first string : ");
+scanf("%s", s1);
+printf("\nEnter the second string :");
+scanf("%s",s2);
+strcat(s1,s2);
+printf("The concatenated string is : %s",s1);
+    return 0;
+}
