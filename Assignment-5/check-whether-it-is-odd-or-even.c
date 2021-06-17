93d2cf5e82c7d23d8f08e67fb6418a831ae83ca4
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-17 12:08:15 +0530

    Create check-whether-it-is-odd-or-even.c

---
+++ Assignment-5/check-whether-it-is-odd-or-even.c
@@ -0,0 +1,16 @@
+/*
+Question : Write a C program to check whether a given number is even or odd.
+*/
+
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+
+    return 0;
+}
