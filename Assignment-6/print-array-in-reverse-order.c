d97fb06d97b66bc0c7fbd6e5e7fe959c84eeb5cf
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-17 12:52:48 +0530

    Update print-array-in-reverse-order.c

--- Assignment-6/print-array-in-reverse-order.c
+++ Assignment-6/print-array-in-reverse-order.c
@@ -2,45 +2,47 @@
 Question:  Write a C program to delete duplicate elements from array.
 */
 
-#include<stdio.h>
-int remove_duplicate_elements(int arr[], int n)
+#include <stdio.h>
+int remove_duplicate(int arr[], int n)
 {
 
-if (n==0 || n==1)
-return n;
+  if (n == 0 || n == 1)
+    return n;
 
-int temp[n];
+  int temp[n];
 
-int j = 0;
-int i;
-for (i=0; i<n-1; i++)
-if (arr[i] != arr[i+1])
-temp[j++] = arr[i];
-temp[j++] = arr[n-1];
+  int j = 0;
+  int i;
+  for (i = 0; i < n - 1; i++)
+    if (arr[i] != arr[i + 1])
+      temp[j++] = arr[i];
+  temp[j++] = arr[n - 1];
 
-for (i=0; i<j; i++)
-arr[i] = temp[i];
+  for (i = 0; i < j; i++)
+    arr[i] = temp[i];
 
-return j;
+  return j;
 }
-
 
 int main()
 {
-int n;
-scanf(“%d”,&n);
-int arr[n];
-int i;
-for(i = 0; i < n; i++)
-{
-scanf(“%d”,&arr[i]);
-}
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  int i;
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+  printf("\nArray Before Removing Duplicates: ");
+  for (i = 0; i < n; i++)
+    printf("%d ", arr[i]);
 
-n = remove_duplicate_elements(arr, n);
+  n = remove_duplicate(arr, n);
 
+  printf("\nArray After Removing Duplicates: ");
+  for (i = 0; i < n; i++)
+    printf("%d ", arr[i]);
 
-for (i=0; i<n; i++)
-printf(“%d “,arr[i]);
-
-return 0;
+  return 0;
 }
