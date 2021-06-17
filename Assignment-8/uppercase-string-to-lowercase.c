16d1a3ebc7500280bcc13edaba524c1fc820d5e9
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-17 15:03:30 +0530

    Create uppercase-string-to-lowercase.c

---
+++ Assignment-8/uppercase-string-to-lowercase.c
@@ -0,0 +1,19 @@
+/*
+Question: Write a C program to covert the uppercase string to lowercase
+*/
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+   char str[25];
+   int i;
+   printf("Enter the string: ");
+   scanf("%s",str);
+
+   for(i=0;i<=strlen(str);i++){
+      if(str[i]>=65&&str[i]<=90)
+         str[i]=str[i]+32;
+   }
+   printf("\nLower Case String is: %s",str);
+   return 0;
+}
