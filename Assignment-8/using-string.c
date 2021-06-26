6527a0d46e8fb6a0602375c74fa77e6ce677df9f
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-26 21:17:40 +0530

    -

--- Assignment-8/using-string.c
+++ Assignment-8/using-string.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include <string.h>
 int main(){
-  char ch[11]={'j', 'a', 'v', 'a', 't', 'p', 'o', 'i', 'n', 't', '\0'};
-   char ch2[11]="javatpoint";
+  char ch[11]={'a', 'i', 's', 'h', 'w', 'a', 'r', 'y', 'a'};
+   char ch2[11]="aishwarya";
 
    printf("Char Array Value is: %s\n", ch);
    printf("String Literal Value is: %s\n", ch2);
  return 0;
-}    
+}
