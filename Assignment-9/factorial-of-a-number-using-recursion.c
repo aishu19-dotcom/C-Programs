bcbd98810b429c58f2a6c969feb61b78211f431c
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-26 23:43:14 +0530

    Create factorial-of-a-number-using-recursion.c

---
+++ Assignment-9/factorial-of-a-number-using-recursion.c
@@ -0,0 +1,20 @@
+/*
+Question : Write a C Program to find Factorial of a given number using recursive function
+*/
+
+#include<stdio.h>
+long int multiplyNumbers(int n);
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    return 0;
+}
+
+long int multiplyNumbers(int n) {
+    if (n>=1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
