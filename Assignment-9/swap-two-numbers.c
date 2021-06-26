21a7f1b493a90531b87e022a5ea145f20ee0ed6a
aishu19-dotcom <77623254+aishu19-dotcom@users.noreply.github.com>
2021-06-26 23:49:57 +0530

    Create swap-two-numbers.c

---
+++ Assignment-9/swap-two-numbers.c
@@ -0,0 +1,18 @@
+/*
+Question : Write a C program to swap two numbers using functions.
+*/
+
+#include<stdio.h>
+int main() {
+      double first, second, temp;
+      printf("Enter first number: ");
+      scanf("%lf", &first);
+      printf("Enter second number: ");
+      scanf("%lf", &second);
+      temp = first;
+      first = second;
+      second = temp;
+      printf("\nAfter swapping, firstNumber = %.2lf\n", first);
+      printf("After swapping, secondNumber = %.2lf", second);
+      return 0;
+}
